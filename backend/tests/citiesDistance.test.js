da8b6d77500e719a9fdf63d009204da2f5d9aee1
ammar <ammar.botonjic@convergent-usa.com>
2020-11-29 18:14:33 +0100

    Tests corrections

--- backend/tests/citiesDistance.test.js
+++ backend/tests/citiesDistance.test.js
@@ -23,14 +23,22 @@ test('Fail if undefined provided', () => {
     expect(getCitiesDirections(undefined).toBeUndefined());
 });
 
-test('Fail if number not provided', () => {
-    expect(getCitiesDirections('hello').toEqual(expect.any(Number))
+test('Fail if coordinate is negative', () => {
+    expect(getCitiesDirections(-44.8683,13.8481,45.6433,13.7903).toThrow())
 });
 
-test('Fail if latitude of the first city is missing', () => {
+test('Fail if string provided', () => {
+    expect(getCitiesDirections('hello').toThrow());
+});
+
+test('Fail if object provided', () => {
+    expect(getCitiesDirections({}).toThrow());
+});
+
+test('Fail if latitude is missing', () => {
     expect(getCitiesDirections(13.8481,45.6433,13.7903).toThrow());
 });
 
-test('Fail if longitude of the first city is missing', () => {
+test('Fail if longitude is missing', () => {
     expect(getCitiesDirections(44.8683,45.6433,13.7903).toThrow());
 });
